8858bbf03e0ee23a0f3abaf41fc2808dbae9e44d
ljpson <ljpson@naver.com>
2023-04-24 16:55:24 +0900

    메인페이지 완성

--- src/pages/VideoCard.jsx
+++ src/pages/VideoCard.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { formatAgo } from '../util/date';
 
 export default function VideoCard({video}) {
+    const { title, thumbnails, channelTitle, publishTime } = video.snippet;
     return (
-        <div>
-            {video.snippet.title}
-        </div>
+        <li>
+            <img className='w-full' src={thumbnails.medium.url} alt={title} />
+            <div>
+                <p className='font-semibold my-2 line-clamp-2'>{title}</p>
+                <p className='text-sm opacity-80'>{channelTitle}</p>
+                <p className='text-sm opacity-80'>{formatAgo(publishTime, 'ko')}</p>
+            </div>
+        </li>
     );
 }
 
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -17,7 +17,8 @@ export default function Videos() {
             <div>Videos : {keyword? `👀${keyword}🔍` : '🔥'}</div>
             {isLoading && <p>Loading...</p>}
             {error && <p>Something is wrong</p>}
-            {videos && <ul>
+            {videos && 
+                <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'>
                 { videos.map(video => 
                     <VideoCard key={video.id} video={video}/>)}
                 </ul>}
